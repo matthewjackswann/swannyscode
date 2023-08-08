54156ebe4800d2e6be2905c570096fe918aaf5bf
Matthew Swann <cv20549@bristol.ac.uk>
2023-08-09 00:14:02 +0100

    started making collapsible scroll into view when off-screen

--- src/Components/Collapsible.jsx
+++ src/Components/Collapsible.jsx
@@ -5,18 +5,23 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 function Collapsible({children, header, className}) {
 
     const divRef = useRef(null);
+    const thisRef = useRef(null);
 
     const toggleDropdown = () => {
         if (!divRef.current.classList.contains("grid-rows-[1fr]")) {
             divRef.current.classList.add("grid-rows-[1fr]");
             divRef.current.classList.remove("grid-rows-[0fr]");
+            // todo only scroll if entire div not already in view
+            setTimeout(() => {
+                thisRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+            }, 100);
         } else {
             divRef.current.classList.add("grid-rows-[0fr]");
             divRef.current.classList.remove("grid-rows-[1fr]");
         }
     }
 
-    return (<div className={className}>
+    return (<div className={"scroll-mt-4 " + className} ref={thisRef}>
         <div className="flex cursor-pointer" onClick={toggleDropdown}>
             {header}
             <FontAwesomeIcon icon={faChevronDown} className="float-right ml-auto p-2"/>
