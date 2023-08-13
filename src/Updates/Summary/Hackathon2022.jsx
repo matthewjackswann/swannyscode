4f4f0ac62eff7f6195a652d7ffd8acafe4812d44
Matthew Swann <cv20549@bristol.ac.uk>
2023-08-13 11:16:36 +0100

    made text sound better

--- src/Updates/Summary/Hackathon2022.jsx
+++ src/Updates/Summary/Hackathon2022.jsx
@@ -1,6 +1,6 @@
 function Hackathon() {
     return (<div>
-        Our submission for the CSS x Boeing Hackathon 2022 with the theme of "Sustainable Cities and Communities". We created a navigation platform that aggregates data from 3rd party APIs and provides real-time information nearby forms of transport. When showing directions multiple forms of transport are shown with a predicted cost and CO2 emissions. We sadly didn't win any prizes but did receive an honourable mention and were invited to showcase our work at a local environmental showcase.
+        Our submission for the CSS x Boeing Hackathon 2022 with the theme of "Sustainable Cities and Communities". We created a navigation platform that aggregates data from 3rd party APIs and provides real-time information nearby forms of transport. When giving directions, multiple forms of transport are shown, each with predicted cost and CO2 emissions. We sadly didn't win any prizes but did receive an honourable mention and were invited to present our work at a local environmental showcase.
     </div>);
 }
 
