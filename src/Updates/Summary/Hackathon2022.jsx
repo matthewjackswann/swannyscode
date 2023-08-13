998bae1216123c18e0feb880a909177077e9d2d0
Matthew Swann <cv20549@bristol.ac.uk>
2023-08-13 01:52:17 +0100

    added green maps writeup

---
+++ src/Updates/Summary/Hackathon2022.jsx
@@ -0,0 +1,7 @@
+function Hackathon() {
+    return (<div>
+        Our submission for the CSS x Boeing Hackathon 2022 with the theme of "Sustainable Cities and Communities". We created a navigation platform that aggregates data from 3rd party APIs and provides real-time information nearby forms of transport. When showing directions multiple forms of transport are shown with a predicted cost and CO2 emissions. We sadly didn't win any prizes but did receive an honourable mention and were invited to showcase our work at a local environmental showcase.
+    </div>);
+}
+
+export default Hackathon;
