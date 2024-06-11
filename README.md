caf397e34367c96a0aff6dae7e5f29578254a7c3
Matthew Swann <cv20549@bristol.ac.uk>
2024-06-11 15:20:24 +0100

    Suppressed bad warnings, fixed lack of colours, added useful command to readme

--- README.md
+++ README.md
@@ -72,3 +72,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+### Useful Commands I use
+
+Convert svg into react components
+```
+svgo *.svg
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,22 @@ export const content = [
   "./src/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
-  extend: {},
-  colors: {
-    'text': '#0b1009',
-    'background': '#f6eff5',
-    'primary': '#5e2071',
-    'secondary': '#b1b1c1',
-    'accent': '#20714d',
-    'background-faded': '#E7E6EB',
-    'text-dark': '#F1EFF6', //https://realtimecolors.com/?colors=F1EFF6-0b1009-5e2071-020203-5ad09b
-    'background-dark': '#0a0910',
-    'primary-dark': '#5e2071',
-    'secondary-dark': '#1c1c2a',
-    'accent-dark': '#35ba7e',
-    'background-faded-dark': '#151215',
-  }
+  extend: {
+    colors: {
+      'text': '#0b1009',
+      'background': '#f6eff5',
+      'primary': '#5e2071',
+      'secondary': '#b1b1c1',
+      'accent': '#20714d',
+      'background-faded': '#E7E6EB',
+      'text-dark': '#F1EFF6', //https://realtimecolors.com/?colors=F1EFF6-0b1009-5e2071-020203-5ad09b
+      'background-dark': '#0a0910',
+      'primary-dark': '#5e2071',
+      'secondary-dark': '#1c1c2a',
+      'accent-dark': '#35ba7e',
+      'background-faded-dark': '#151215',
+    }
+  },
 };
 export const plugins = [];
 export const darkMode = 'class';
