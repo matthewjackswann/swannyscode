093db3afd1cff54d3c9aee588fd2afcb5f52009f
Matthew Swann <cv20549@bristol.ac.uk>
2024-06-11 15:21:16 +0100

    added brishack update

--- src/Updates/Summary/BrisHack2024.jsx
+++ src/Updates/Summary/BrisHack2024.jsx
@@ -1,7 +1,7 @@
 function BrisHack() {
     return (<div>
         For this years theme of "health", my team created a webapp which created spotify playlists for running. You could log in using spotify and select a route length and speed. It created a route for you and a playlist to match the speed you wanted to run at. The app was built using React and Flask and we ended up winning the "Best Software" category.
-        <iframe className="mx-auto aspect-video h-full w-full" title="spotify app video" width="720" height="520" src="https://www.youtube.com/watch?v=9MRcfe_n8jI" />
+        <iframe className="mx-auto aspect-video h-full w-full" title="spotify app video" width="720" height="520" src="https://www.youtube.com/embed/9MRcfe_n8jI?si=39xID-v2Ct2-cmR9" />
     </div>);
 }
 
